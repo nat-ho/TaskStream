0e5f4cd09be1eb5dc727c05d422fef9918e7bf23
Ho Chong Han Nathaniel <e0407282@u.nus.edu>
2020-09-18 17:01:35 +0800

    Print data using streams in main

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -22,6 +22,10 @@ public class Main {
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+        printDeadlinesUsingStreams(tasksData);
+
+        System.out.println("Total number of deadline using streams: " + countDeadlinesUsingStreams(tasksData));
+
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
